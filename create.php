82e71e58b6808a08a4626ef67cf4384438f83c38
Hanspeter Spalinger <hanfi@spahan.ch>
2018-05-20 14:36:43 +0200

    removed recaptchalib

--- create.php
+++ create.php
@@ -1,7 +1,6 @@
 <?php
 
 require_once('snacom/config.php');
-require_once('snacom/recaptchalib.php');
 
 $uid = strtolower(filter_input(INPUT_POST, 'uid', FILTER_VALIDATE_REGEXP, $VALID_USER_FILTER));
 if (!$uid) { header('HTTP/1.1 400 Bad Request'); die(); }
